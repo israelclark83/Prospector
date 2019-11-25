ddb9733df43a386d18dc9ce26d855dba039a75ae
israelclark83 <54966452+israelclark83@users.noreply.github.com>
2019-11-25 00:41:46 -0500

    Created CardProspector Script

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -17,6 +17,7 @@ public class Prospector : MonoBehaviour {
     [Header("Set Dynamically")]
 	public Deck					deck;
     public Layout               layout;
+    public List<CardProspector> drawPile;
 
     void Awake(){
 		S = this;
@@ -36,6 +37,20 @@ public class Prospector : MonoBehaviour {
 
         layout = GetComponent<Layout>();  // Get the Layout component
         layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;                                     // a
+            lCP.Add(tCP);
+        }
+        return (lCP);
     }
 
 }
